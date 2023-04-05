dc4e9da44cc18b884b36be767c3ec7a7282a7124
Selena Small <selenawiththetattoo@gmail.com>
2023-04-06 00:00:51 +1000

    make the led light flash
    Co-authored-by: Michael Milewski <saramic@gmail.com>

--- apps/mruby_actuator/main/spiffs/main.rb
+++ apps/mruby_actuator/main/spiffs/main.rb
@@ -6,10 +6,19 @@ wifi.connect("loki", "password") # TODO: how best to externalise secrets
 
 puts "Connected"
 
+led = ESP32::GPIO::GPIO_NUM_2 # ESP32 default led pin
+ESP32::GPIO::pinMode(led, ESP32::GPIO::OUTPUT)
+
 mqtt = ESP32::MQTT::Client.new('failure-driven.local', 1883)
 mqtt.connect
 mqtt.publish("kick/manage", '{ "hello": "world." }')
 mqtt.disconnect
+
+def kick(led)
+  ESP32::GPIO::digitalWrite(led, ESP32::GPIO::HIGH)
+  ESP32::System.delay(200)
+  ESP32::GPIO::digitalWrite(led, ESP32::GPIO::LOW)
+end
 
 #
 # Loop forever otherwise the script ends
@@ -18,4 +27,5 @@ while true do
   mem = ESP32::System.available_memory() / 1000
   puts "Free heap: #{mem}K"
   ESP32::System.delay(10000)
+  kick(led)
 end
