eed94d4c61f00bf629de408705064acb6fb4b470
Michael Milewski <saramic@gmail.com>
2023-04-27 08:32:44 -0400

    Basic ESP32 blink example

---
+++ apps/examples/esp32_blink/esp32_blink.ino
@@ -0,0 +1,12 @@
+#define LED_BUILTIN 2           // ESP32 builtin led
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
