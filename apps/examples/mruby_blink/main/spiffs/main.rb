2d044ef94b504179cd925a8247557ae61df1275a
Michael Milewski <saramic@gmail.com>
2023-05-02 18:37:39 +1000

    rubocop fix call to method

--- apps/examples/mruby_blink/main/spiffs/main.rb
+++ apps/examples/mruby_blink/main/spiffs/main.rb
@@ -5,9 +5,8 @@ led = ESP32::GPIO::GPIO_NUM_2 # ESP32 default led pin
 ESP32::GPIO.pinMode(led, ESP32::GPIO::OUTPUT)
 
 loop do
-  ESP32::GPIO::digitalWrite(led, ESP32::GPIO::HIGH)
+  ESP32::GPIO.digitalWrite(led, ESP32::GPIO::HIGH)
   ESP32::System.delay(1000)
-  ESP32::GPIO::digitalWrite(led, ESP32::GPIO::LOW)
+  ESP32::GPIO.digitalWrite(led, ESP32::GPIO::LOW)
   ESP32::System.delay(1000)
 end
-
